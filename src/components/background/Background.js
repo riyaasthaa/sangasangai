fc7e6b272487d771cb85a5401a1d4882bbdd6ab3
Suwas52 <suwasdanuwar18@gmail.com>
2023-09-17 22:19:55 +0545

    update background remaining

--- src/components/background/Background.js
+++ src/components/background/Background.js
@@ -4,34 +4,40 @@ import shape from "../../img/shape-grid.png";
 
 const Background = () => {
   return (
-    <div className="background d-flex justify-content-between ">
-      <div className=" left ">
-        <img src={shape} alt="" />
-        <div className="box align-items-center justify-content-center"></div>
-      </div>
-      <div className=" right">
-        <img src={shape} alt="" />
-        <div className="box">
-          <img src="" alt="" />
+    // <div className="background d-flex justify-content-between ">
+    //   <div className=" left ">
+    //     <img src={shape} alt="" />
+    //     <div className="box align-items-center justify-content-center"></div>
+    //   </div>
+    //   <div className=" right">
+    //     <img src={shape} alt="" />
+    //     <div className="box">
+    //       <img src="" alt="" />
+    //     </div>
+    //   </div>
+    //   <div className="center">
+
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="background">
+      <img className="img-left" src={shape} alt="" />
+      <div className="content">
+        <h2 className="heading">Background</h2>
+        <div className="description">
+          <p className="text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui,
+            laboriosam mollitia ducimus voluptatibus distinctio harum ad
+            expedita nihil. Doloremque perspiciatis iure autem sunt architecto,
+            dolores error excepturi eos dignissimos dolorem reiciendis, alias
+            voluptatibus eaque? Nostrum iusto tempore quia quod doloremque
+            explicabo quae atque facilis, eveniet, similique impedit aspernatur
+            consequuntur. In, laudantium id ipsa quisquam suscipit reprehenderit
+            eum accusantium quis minus omnis
+          </p>
         </div>
       </div>
-      <div className="center">
-        <div className="container justify-content-center mt-2">
-          <h2 className="title">Background</h2>
-          <div className="description">
-            <p className="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui,
-              laboriosam mollitia ducimus voluptatibus distinctio harum ad
-              expedita nihil. Doloremque perspiciatis iure autem sunt
-              architecto, dolores error excepturi eos dignissimos dolorem
-              reiciendis, alias voluptatibus eaque? Nostrum iusto tempore quia
-              quod doloremque explicabo quae atque facilis, eveniet, similique
-              impedit aspernatur consequuntur. In, laudantium id ipsa quisquam
-              suscipit reprehenderit eum accusantium quis minus omnis
-            </p>
-          </div>
-        </div>
-      </div>
+      <img className="img-right" src={shape} alt="" />
     </div>
   );
 };
