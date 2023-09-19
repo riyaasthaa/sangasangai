e891b3279f54fdd0775d3b2dfe1f292bbb699ac6
Riya <riyaaaa86@gmail.com>
2023-09-19 10:53:39 +0545

    installed gh-pages and basic setup done for publishing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/sangasangai">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
