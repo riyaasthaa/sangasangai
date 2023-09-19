918fba1e950d8aad199c14b410cf02315476d974
Riya <riyaaaa86@gmail.com>
2023-09-19 09:43:16 +0545

    github page update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/SanghSangai">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
